0ea8cec9eba772c2f4e674353b87dd0949f033e0
eziod <ezio.decadt@epitech.eu>
2023-11-30 11:13:43 +0100

    [~] Add checking for screen size

--- src/game/loop.c
+++ src/game/loop.c
@@ -12,12 +12,18 @@
 void game_loop(map_t *map)
 {
     int key = 0;
+    int rows;
+    int cols;
 
     while (key != 27) {
         clear();
+        getmaxyx(stdscr, rows, cols);
         handle_input(map, key);
         check_game(map);
-        display_map(map);
+        if (cols >= map->col && rows >= map->row)
+            display_map(map);
+        else
+            mvprintw(rows / 2, 0, "Please enlarge the terminal");
         key = getch();
         refresh();
     }
